c57f08c60842721c091020ed4ff7e1b0dc2a766f
Gregor Kosmina <kosmina.gregor@gmail.com>
2019-09-21 21:41:22 +0200

    styling changes

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    $('#disclaimer').modal();
+    $('#impressum').modal();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        indicators: true,
+        fullWidth: true,
+        noWrap: true
+    });
+});
+
+$(document).ready(function (listener) {
+
+    $('.button-collapse').sideNav();
+
+    $('.scrollspy').scrollSpy();
+
+    const options = [
+        {
+            selector: '.main-text', offset: 0, callback: function (el) {
+                Materialize.fadeInImage($(el));
+            }
+        }
+    ];
+    Materialize.scrollFire(options);
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 300) {
+            $('nav').removeClass('transparent trans');
+            $('nav').addClass('dark-blue solid');
+        } else {
+            $('nav').addClass('transparent trans');
+            $('nav').removeClass('dark-blue solid');
+        }
+    });
+});
+
+const position = [48.216274, 16.3915678];
+const centerPosition = [48.2169578, 16.4230718];
+
+function mapsInit() {
+
+    const latLng = new google.maps.LatLng(position[0], position[1]);
+    const centerLatLng = new google.maps.LatLng(centerPosition[0], centerPosition[1]);
+
+
+    const mapOptions = {
+        zoom: 14,
+        streetViewControl: false,
+        scaleControl: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: centerLatLng
+    };
+
+    map = new google.maps.Map(document.getElementById('googlemap'), mapOptions);
+    marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'Hedwiggasse 4/26, 1020 Wien',
+        draggable: false,
+        animation: google.maps.Animation.DROP
+    });
+}
